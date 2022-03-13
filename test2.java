ffbf30945ada8891d4776cfb952a8722b7eaa058
TSSSVIKAS <vikas.teeparthi@gmail.com>
2022-03-13 20:54:48 +0530

    deleted test2.java "2nd update in mb"
    This reverts commit a1d6c585a49c500a488194e869ad92c7536abec4.

--- test2.java
+++ test2.java
@@ -1,3 +1,2 @@
-This Line is written in Master Branch
-This is updated in dev branch - contain error
-This is 2nd update in master branch
+This Line is written in Dev Branch
+This line is updated in master branch
